f9cb0295979d76517b43ef89464c47d6b9f34baf
bauerbrun0 <bauerbruno@protonmail.com>
2025-11-09 14:04:25 +0100

    feat(frontend): log out on token expired response

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,19 @@
-import { up } from 'up-fetch'
+import { isResponseError, up } from 'up-fetch'
 import { userStore } from '@/stores/user'
+import { errorSchema } from '@/utils/zod'
 
 export const upfetch = up(fetch, () => ({
   baseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
   headers: { Authorization: userStore.token ? `Bearer ${userStore.token}` : undefined },
+  onError: (error) => {
+    if (!isResponseError(error)) {
+      throw error
+    }
+    if (error.status === 403) {
+      const res = errorSchema.safeParse(error.data)
+      if (res.success && res.data.error === 'Token expired') {
+        userStore.logout()
+      }
+    }
+  },
 }))
