8a0877ea58d2210225d4631ce37cb8a0b5f545ee
bauerbrun0 <bauerbruno@protonmail.com>
2025-11-09 13:14:11 +0100

    feat(frontend): add link to header logo

--- frontend/src/components/header/SnippetVaultLogo.vue
+++ frontend/src/components/header/SnippetVaultLogo.vue
@@ -3,8 +3,10 @@ import CodeIcon from '@/icons/CodeIcon.vue'
 </script>
 
 <template>
-  <div class="flex items-center gap-3 mr-4">
-    <CodeIcon classes="stroke-primary-500 h-7 w-7" />
-    <h1 class="text-lg font-semibold">SnippetVault</h1>
-  </div>
+  <router-link to="/snippets">
+    <div class="flex items-center gap-3 mr-4">
+      <CodeIcon classes="stroke-primary-500 h-7 w-7" />
+      <h1 class="text-lg font-semibold">SnippetVault</h1>
+    </div>
+  </router-link>
 </template>
