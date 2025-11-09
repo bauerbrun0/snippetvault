26f8452607e4b228e2b9a1d371fe0085e29dd90b
bauerbrun0 <bauerbruno@protonmail.com>
2025-11-09 14:25:25 +0100

    chore(api): load frontend url from application.yaml

--- api/src/main/java/org/bauerbrun0/snippetvault/api/config/WebConfig.java
+++ api/src/main/java/org/bauerbrun0/snippetvault/api/config/WebConfig.java
@@ -8,10 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    private final AppProperties appProperties;
+
+    public WebConfig(AppProperties appProperties) {
+        this.appProperties = appProperties;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(this.appProperties.getFrontend())
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
